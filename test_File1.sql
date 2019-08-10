370662ed7de78e88f2bf4b4a8af42fd202980ee5
deepaK <deepaK@idea-PC>
2019-08-10 15:55:00 +0530

    test commit

--- test_File1.sql
+++ test_File1.sql
@@ -1,3 +1,5 @@
 create table t1 ( id1 number(10), name1 varchar2(100), timestamp1 timestamp(6));
 /
 insert into t1 values (101,'Deepak Kumar' , systimestamp);
+/
+--new changes applied hope so will loved it so is this chnaged at all??
